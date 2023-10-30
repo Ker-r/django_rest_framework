bdc8ac683cf440d473843ff743bebbe30d222b61
Каролина Ефремова <efremova.karolina@gmail.com>
2023-10-30 13:19:51 +0700

    Change readme

--- pets/pets/urls.py
+++ pets/pets/urls.py
@@ -30,18 +30,19 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/auth', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-    path(
-        'api/v1/token/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'api/v1/token/refresh/', TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path(
-        'api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'
-    ),
-    # path('api/v1/drf-auth', include('rest_framework.urls')),
+    # path(
+    #     'api/v1/token/', TokenObtainPairView.as_view(),
+    #     name='token_obtain_pair'
+    # ),
+    # path(
+    #     'api/v1/token/refresh/', TokenRefreshView.as_view(),
+    #     name='token_refresh'
+    # ),
+    # path(
+    #     'api/v1/token/verify/', TokenVerifyView.as_view(),
+    #      name='token_verify'
+    # ),
+    path('api/v1/drf-auth', include('rest_framework.urls')),
     # path('api/v1/animallist/', AnimalViewSet.as_view({'get': 'list'})),
     # path(
     # 'api/v1/animallist/<int:pk>', AnimalViewSet.as_view({'put': 'update'})),
